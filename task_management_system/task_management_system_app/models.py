4be3b5be40cfa3c0e98ec55bdfc76ab40c6e186c
Полина <lovepolina2004@gmail.com>
2024-12-24 13:46:04 +0300

    upd

---
+++ task_management_system/task_management_system_app/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('planned', 'Запланировано'),
+        ('in_progress', 'В работе'),
+        ('completed', 'Завершено'),
+    ]
+
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    priority = models.IntegerField(default=1)
+    description = models.TextField(default='')
+    location = models.CharField(max_length=255, default='')
+    organizer = models.CharField(max_length=100, default='')
+    is_completed = models.BooleanField(default=False)  # Можно удалить, если больше не используется
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='planned'
+    )
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.SET_NULL, null=True, blank=True, related_name='notifications')
+    message = models.CharField(max_length=255)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Notification for {self.user.username} - {self.task.name}"
